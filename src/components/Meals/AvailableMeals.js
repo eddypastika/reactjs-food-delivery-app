5d05a462a7e6a2b40a84532ed2b93b2a3071be7b
eddypastika <pastikaeddy@gmail.com>
2021-06-16 21:15:45 +0700

    add MealItemForm component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,33 +5,40 @@ import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
     {
         id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
+        name: 'Chicken Betutu',
+        description: 'Balinese Chicken with traditional recipe',
+        price: 17.99,
     },
     {
         id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
+        name: 'Satay Lilit',
+        description: 'Balinese fish satay with coconut',
+        price: 18.5,
     },
     {
         id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
+        name: 'Babi Guling',
+        description: 'Balinese pork roast',
+        price: 25.99,
     },
     {
         id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
+        name: 'Nasi Goreng Jawa',
+        description: 'Javanese Fried Rice',
+        price: 12.99,
+    },
+    {
+        id: 'm5',
+        name: 'Rendang',
+        description: 'Beef from Minang',
+        price: 22.99,
     },
 ];
 
 const AvailableMeals = () => {
     const meals = DUMMY_MEALS.map(meal => (
         <MealItem
+            id={meal.id}
             key={meal.id}
             name={meal.name}
             description={meal.description}
