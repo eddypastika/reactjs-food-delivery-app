8aa6a89603b3bfe34621f4f9c3575e949d69be70
eddypastika <pastikaeddy@gmail.com>
2021-07-26 17:36:59 +0700

    add http fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,42 +1,33 @@
+import React, {useEffect, useState} from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Chicken Betutu',
-        description: 'Balinese Chicken with traditional recipe',
-        price: 17.99,
-    },
-    {
-        id: 'm2',
-        name: 'Satay Lilit',
-        description: 'Balinese fish satay with coconut',
-        price: 18.5,
-    },
-    {
-        id: 'm3',
-        name: 'Babi Guling',
-        description: 'Balinese pork roast',
-        price: 25.99,
-    },
-    {
-        id: 'm4',
-        name: 'Nasi Goreng Jawa',
-        description: 'Javanese Fried Rice',
-        price: 12.99,
-    },
-    {
-        id: 'm5',
-        name: 'Rendang',
-        description: 'Beef from Minang',
-        price: 22.99,
-    },
-];
-
 const AvailableMeals = () => {
-    const meals = DUMMY_MEALS.map(meal => (
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const resp = await fetch("https://react-http-c60f9-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json");
+            const respData = await resp.json();
+
+            const loadedMeals = [];
+            for (const key in respData) {
+                loadedMeals.push({
+                    id: key,
+                    name: respData[key].name,
+                    description: respData[key].description,
+                    price: respData[key].price,
+                });
+            }
+
+            setMeals(loadedMeals);
+        };
+
+        fetchMeals();
+    }, []);
+
+    const mealsData = meals.map(meal => (
         <MealItem
             id={meal.id}
             key={meal.id}
@@ -49,7 +40,7 @@ const AvailableMeals = () => {
     return (
         <section className={classes.meals}>
             <Card>
-                <ul>{meals}</ul>
+                <ul>{mealsData}</ul>
             </Card>
         </section>
     )
